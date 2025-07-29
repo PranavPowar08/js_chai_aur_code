63986bc6b12dc840c784404876cafe7003025501
Pranav <112745951+PranavPowar08@users.noreply.github.com>
2025-07-29 07:00:01 +0000

    Day 1 Work

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=214568
+var accountEmail ="pranav@google.com"
+let accountPassword ="12345678"
+accountCity ="Jaipur"
+
+// accountId=2  //It gives error cause value cant be  changed of const
+
+console.log(accountId)
+
+accountEmail="a@a.com"
+accountPassword="121212"
+accountCity="Kolhapur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
