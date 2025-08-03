924971f5cb1ea8eb0e6781a299bbd50b881aa623
PranavPowar08 <pranavpowar8669@gmail.com>
2025-08-03 19:16:04 +0530

    Camparisons of Datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,16 @@
 console.log("Pranav")
 
+let score =33
+
+console.log(typeof score)
+console.log(typeof(score))
+
+score ="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let numValue = Number(score)
+console.log(typeof numValue)
+
+console.log(13%5);
