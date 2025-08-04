57a1d47766f8054a4bcf94553fdb371624d06b07
PranavPowar08 <pranavpowar8669@gmail.com>
2025-08-04 22:09:18 +0530

    loops

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,16 +1,56 @@
-console.log("Pranav")
+// console.log("Pranav")
 
-let score =33
+// let score =33
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-score ="33"
+// score ="33"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let numValue = Number(score)
-console.log(typeof numValue)
+// let numValue = Number(score)
+// console.log(typeof numValue)
 
-console.log(13%5);
+// console.log(13%5);
+
+console.log("For Loop")
+for(let i =0; i<5; i++){
+    console.log(i*2)
+}
+
+
+console.log("While Loop")
+let i = 1
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+console.log("Do_While Loop")
+
+let j=1
+do{
+    console.log(j);
+    j++;
+
+}while(j<5);
+
+console.log("For ..in Loop")
+const person ={
+    name:"Pranav",
+    age:22,
+    city:"Kolhapur"
+}
+
+for(let info in person){
+    console.log(info,person[info])
+}
+
+
+console.log("For ..of Loop")
+let arr =[1,2,3,4,5]
+for(let val of arr){
+    console.log(val)
+}
