5d8f9f519004014fe54e1fa86a8d1a6cc3814862
PranavPowar08 <pranavpowar8669@gmail.com>
2025-08-04 13:14:24 +0530

    String Functions

---
+++ 01_basics/05_string.js
@@ -0,0 +1,39 @@
+const name = "Pranav"
+const repocount = 50
+
+// console.log(name + repocount);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
+
+// console.log("Hello")
+const gamename =new String('PranavHc')
+
+// console.log(gamename[1])
+// console.log(gamename.__proto__)
+// console.log(gamename.length)
+
+// console.log(gamename.toUpperCase())
+// console.log(gamename.charAt(2))
+// console.log(gamename.indexOf("a"))
+
+const newname = gamename.substring(0,4)
+// console.log(newname)
+
+
+const anothername = gamename.slice(-5,4)
+// console.log(anothername)
+
+const newstring ="      Pranav    "
+console.log(newstring +"H")
+console.log(newstring.trim())
+
+const url ="https://www.youtube.com/watch%200index=11"
+console.log(url)
+console.log(url.replace("%200","pranav"))
+
+
+
+
+
+
+
