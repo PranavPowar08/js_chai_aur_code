79829efbc034449a644eaef53f0bb331517dd0dc
PranavPowar08 <pranavpowar8669@gmail.com>
2025-08-06 14:10:22 +0530

    Swith Case

---
+++ 04_ControlFlow/02_switch.js
@@ -0,0 +1,30 @@
+// switch(key){
+//     case varl:
+//         break;
+//     case var2:
+//         break;
+//     default:
+//         break;
+// }
+
+const month =3
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    case 5:
+        console.log("May")
+        break;
+    default:
+        console.log("Value not in 1-5")
+        break;
+}
